42d0804b8d628a4246fce68ee4e0bc6f509f4073
Andrew Eames <ajerxs@gmail.com>
2021-08-01 15:51:04 -0400

    Added Users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,36 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
-    erb :index
+    # if logged_in?
+    #   redirect '/somewhere'
+    # else
+      erb :index
+    # end
   end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    end
+    # redirect '/somewhere'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 
 end
