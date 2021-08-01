708cca6bd51c80c980dfc330131230609fab9986
Andrew Eames <ajerxs@gmail.com>
2021-08-01 19:40:24 -0400

    Show page for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,17 @@
 class GamesController < ApplicationController
 
+    get '/games' do 
+        unordered_games = []
+        Game.all.each do |game|
+            unordered_games << game.title
+        end
+        @games = unordered_games.sort
+        erb :'games/show'
+    end
+
+    get '/games/:slug' do 
+        @game = Game.find_by_slug(params[:slug])
+        erb :'games/game'
+    end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,14 @@
 require_relative 'concerns/slugify'
 
 class Game < ActiveRecord::Base
+    extend Slugify::ClassMethods
+
     has_many :achievements
     has_many :user_games
     has_many :users, through: :user_games
+
+    def slug
+        the_name = self.title
+        slug = the_name.downcase.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
 end
