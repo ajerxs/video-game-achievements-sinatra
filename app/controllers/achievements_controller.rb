b642250830ab42d1a46994b62e26165f2677174e
Andrew Eames <ajerxs@gmail.com>
2021-08-04 18:13:53 -0400

    New achievement functionality added

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,8 +1,43 @@
 class AchievementsController < ApplicationController
 
     get '/achievements' do
-        @achievements = Achievement.all
+        unsorted_achievement_names = []
+        @achievements = []
+        Achievement.all.each do |achievement|
+            unsorted_achievement_names << achievement.name
+        end
+        sorted_achievement_names = unsorted_achievement_names.sort
+        sorted_achievement_names.each do |achievement|
+            @achievements << Achievement.find_by(name: achievement)
+        end
         erb :'achievements/show'
+    end
+
+    get '/achievements/new' do 
+        @games = Game.all
+        erb :'achievements/new'
+    end
+
+    post '/achievements' do
+        @user = current_user
+
+        if params[:game][:id] != ""
+            @game = Game.find_by_id(params[:game][:id])
+        elsif params[:game_title] != ""
+            @game = Game.create(title: params[:game_title])
+        else
+            redirect '/achievements/new'
+        end
+        
+
+        if params[:achievement_name] != "" && params[:achievement_description] != "" && @game
+            @achievement = Achievement.create(name: params[:achievement_name], description: params[:achievement_description])
+            @game.achievements << @achievement
+            @user.achievements << @achievement
+            redirect '/achievements'
+        else
+            redirect '/achievements/new'
+        end
     end
 
     get '/achievements/:id' do 
