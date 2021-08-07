2dd447485dd1d3acfa7ee3a4c754c86ed831fd8e
Andrew Eames <ajerxs@gmail.com>
2021-08-07 13:44:37 -0400

    Post functionality implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,36 @@ class PostsController < ApplicationController
     end
 
     post '/posts' do
-
+        @user = current_user
+        @post = Post.create(message: params[:post_message], user_id: @user.id)
+        redirect '/posts'
     end
 
     get '/posts/new' do 
         erb :'posts/new'
+    end
+
+    get '/posts/:id' do 
+        @post = Post.find_by_id(params[:id])
+        erb :'posts/post'
+    end
+
+    get '/posts/:id/edit' do
+        @post = Post.find_by_id(params[:id])
+        erb :'posts/edit'
+    end
+
+    patch '/posts/:id' do 
+        @post = Post.find_by_id(params[:id])
+        @post.update(message: params[:post_message])
+        @post.save
+        redirect "/posts/#{@post.id}"
+    end
+
+    post '/posts/:id/delete' do 
+        @post = Post.find_by_id(params[:id])
+        @post.delete
+        redirect '/posts'
     end
 
 end
